a974cf46349eaa2251fc96557512a0cdd826f342
Yuvaraj <shivarajnaidu@gmail.com>
2018-07-02 09:35:53 +0530

    Added mailing with send grid and dotenv

--- app/lib/send-mail.js
+++ app/lib/send-mail.js
@@ -1,9 +1,21 @@
 'use strict';
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function send(to, mailOptions) {
+
+	const defaultOptions = {
+        from: 'test@example.com',
+        subject: 'Test Email Subject'
+	};
+
+	if (!to) {
+		throw new Error('To Address Should Not Be Empty');
+	}
+
+	const message = Object.assign({}, defaultOptions, mailOptions);
+    return sgMail.send(message);
+}
 
 
-
-
-
-module.exports = {
-
-};
+module.exports = send;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const PORT = process.env.NODE_PORT || 3000;
 const cors = require('cors');
