2c50ce2dce84a7e45e7d1f1b17189cce04e447f4
Yuvaraj <shivarajnaidu@gmail.com>
2019-06-04 19:46:02 +0530

    cleanups

--- app/lib/password.js
+++ app/lib/password.js
@@ -1,27 +1,29 @@
-'use strict';
+
+
 const bcrypt = require('bcrypt');
+
 const saltRounds = 10;
 
 async function hash(passWord) {
-    if (!passWord) {
-        const error = TypeError('Password Should Not Be Empty');
-        throw error;
-    }
+  if (!passWord) {
+    const error = TypeError('Password Should Not Be Empty');
+    throw error;
+  }
 
-    return await bcrypt.hash(passWord, saltRounds); // This will return promise..
+  return bcrypt.hash(passWord, saltRounds); // This will return promise..
 }
 
 async function match(plainPassword, hashedPassword) {
-    if (!plainPassword || !hashedPassword) {
-        const error = TypeError('Password/Hash Should Not Be Empty');
-        throw error;
-    }
+  if (!plainPassword || !hashedPassword) {
+    const error = TypeError('Password/Hash Should Not Be Empty');
+    throw error;
+  }
 
-    return await bcrypt.compare(plainPassword, hashedPassword); // This will return promise..
+  return bcrypt.compare(plainPassword, hashedPassword); // This will return promise..
 }
 
 
 module.exports = {
-    hash,
-    match
+  hash,
+  match,
 };
