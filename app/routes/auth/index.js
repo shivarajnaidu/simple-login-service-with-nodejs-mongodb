57e8966ae3c2116bd00aa1b670491b39807848ae
yuvaraj <shivarajnaidu@gmail.com>
2019-10-06 13:47:29 +0530

    now the otp will be verified and the user account will be moved to users

--- app/routes/auth/index.js
+++ app/routes/auth/index.js
@@ -6,10 +6,12 @@ const signupRouter = require('./signup');
 const forgotPasswordRouter = require('./forgot-password');
 const resetPasswordRouter = require('./reset-password');
 const googleLoginRouter = require('./google-login');
+const otpRouter = require('./otp');
 
 router.use('/login', loginRouter);
 router.use('/google', googleLoginRouter);
 router.use('/signup', signupRouter);
+router.use('/otp', otpRouter);
 router.use('/forgot-password', forgotPasswordRouter);
 router.use('/reset-password', resetPasswordRouter);
 
