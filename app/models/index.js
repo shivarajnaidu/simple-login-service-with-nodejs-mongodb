aacc71c20ffb1aa7a30aabac7f6f10ef80648b85
Nagaraj Vemula <nagaraj.v@HCL.COM>
2019-10-22 11:39:12 +0530

    added access token model

--- app/models/index.js
+++ app/models/index.js
@@ -2,10 +2,12 @@ const db = require('./db');
 const User = require('./user');
 const UnVerifiedAccount = require('./unverified-account');
 const Otp = require('./otp');
+const AccessToken = require('./access-token');
 
 module.exports = {
   db,
   User,
   UnVerifiedAccount,
   Otp,
+  AccessToken
 };
